05f39633354b8621c1027c977bc0c399f6ecd962
Vijay-creater <k.b.vijay61111@gmail.com>
2024-07-15 00:51:22 +0530

    First Commit
    First date: 15-07-2024

---
+++ Zomato_Clone-master/client/src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+// import Details from './Components/DetailsPage/DatilsPage';
+// import Home from './Components/Homepage/Home';
+// import Filter from './Components/FilterPage/Filter';
+import Router from './Components/Router/Router';
+
+function App() {
+  return (
+    <Router/>
+  );
+}
+
+export default App;
